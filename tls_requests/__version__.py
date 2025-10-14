7857e571ce0e5d5f6655a4c7950a2f59a425d24e
Two Dev <thetwofarm@gmail.com>
2025-10-14 18:15:28 +0700

    v1.1.6

--- tls_requests/__version__.py
+++ tls_requests/__version__.py
@@ -5,5 +5,5 @@ __description__ = (
 __url__ = "https://github.com/thewebscraping/tls-requests"
 __author__ = "Tu Pham"
 __author_email__ = "thetwofarm@gmail.com"
-__version__ = "1.1.5"
+__version__ = "1.1.6"
 __license__ = "MIT"
